eed7ae7eec64f571ed574a1f46e0d4dd59c45d23
Bhargav <bhargav04cp657@gmail.com>
2020-01-02 22:31:38 +0530

    std::generate() function is created

--- C++11/main.cpp
+++ C++11/main.cpp
@@ -5,13 +5,22 @@
 #include <stdlib.h>
 using namespace std;
 
-int main()
+
+/* This function uses the std::generate() method to generate the data*/
+void test_std_generate()
 {
+    std::cout<<"This is a test for the std::generate()\n";
     vector<int> v(10);
-    std::generate(begin(v),end(v),[](){return rand();});
+
+    std::generate(begin(v),end(v),[](){return (rand()/10);}); /* use of the lambda function*/
 
     for(auto a:v)
     {
         std::cout<<a<<"\n";
     }
+}
+
+int main()
+{
+    test_std_generate();
 }
